aeced4a5fabb7810b0835b933c4ab0efd76bcb17
croc4-20 <119327900+croc4-20@users.noreply.github.com>
2025-01-15 12:29:25 +0100

    l

--- Build/robots.framework.js
+++ Build/robots.framework.js
@@ -1284,10 +1284,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7568928: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7568983: ($0) => { performance.now = function() { return $0; }; },  
- 7569031: ($0) => { performance.now = function() { return $0; }; },  
- 7569079: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7568960: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7569015: ($0) => { performance.now = function() { return $0; }; },  
+ 7569063: ($0) => { performance.now = function() { return $0; }; },  
+ 7569111: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
