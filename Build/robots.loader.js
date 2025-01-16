39f69a63302c490db0e4a491eca2ae9a26cea947
croc4-20 <119327900+croc4-20@users.noreply.github.com>
2025-01-16 10:48:22 +0100

    rbt

--- Build/robots.loader.js
+++ Build/robots.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 96746698,
+    wasmFileSize: 96746836,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
