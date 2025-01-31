730537befd861d6ced21b152ace10d6f9926caee
croc4-20 <119327900+croc4-20@users.noreply.github.com>
2025-01-31 10:06:14 +0100

    username resolved

--- robots/Build/robots.loader.js
+++ robots/Build/robots.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 94312232,
+    wasmFileSize: 94312271,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
