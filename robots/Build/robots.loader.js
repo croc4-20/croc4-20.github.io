ff13c74b0a2c804e06d20b5fc1ac7bf6196ee0fc
croc4-20 <119327900+croc4-20@users.noreply.github.com>
2025-02-05 14:47:39 +0100

    almost just testing

--- robots/Build/robots.loader.js
+++ robots/Build/robots.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 94327636,
+    wasmFileSize: 94329483,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
