a26d53c7b6b90c159603074246ce6374a207505d
croc4-20 <119327900+croc4-20@users.noreply.github.com>
2025-02-09 09:42:43 +0100

    notifyendmatch test

--- Build/croc4-20.github.io.loader.js
+++ Build/croc4-20.github.io.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 94331370,
+    wasmFileSize: 94331380,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
