ea63d82b5be720b543d57ce30883ed6ec99f372c
croc4-20 <119327900+croc4-20@users.noreply.github.com>
2025-02-11 20:35:09 +0100

    size

--- Build/croc4-20.github.io.framework.js
+++ Build/croc4-20.github.io.framework.js
@@ -1284,10 +1284,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7491712: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7491767: ($0) => { performance.now = function() { return $0; }; },  
- 7491815: ($0) => { performance.now = function() { return $0; }; },  
- 7491863: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7491744: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7491799: ($0) => { performance.now = function() { return $0; }; },  
+ 7491847: ($0) => { performance.now = function() { return $0; }; },  
+ 7491895: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- Build/croc4-20.github.io.loader.js
+++ Build/croc4-20.github.io.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 94340213,
+    wasmFileSize: 94340393,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
