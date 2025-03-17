9a5677f926f89c186805489153e503d22bfa973c
croc4-20 <119327900+croc4-20@users.noreply.github.com>
2025-03-17 11:11:27 +0100

    ttt

--- Build/croc4-20.github.io.loader.js
+++ Build/croc4-20.github.io.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 94422955,
+    wasmFileSize: 94423012,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
