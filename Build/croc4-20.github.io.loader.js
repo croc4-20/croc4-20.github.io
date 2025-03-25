cc650a3415c7199ccb93785bd7ad06be48421f47
croc4-20 <119327900+croc4-20@users.noreply.github.com>
2025-03-25 08:59:37 +0100

    d
    photonmanagerscript onjoinedroom
    changes for instantiating custom robot

--- Build/croc4-20.github.io.loader.js
+++ Build/croc4-20.github.io.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 94433343,
+    wasmFileSize: 94430898,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
